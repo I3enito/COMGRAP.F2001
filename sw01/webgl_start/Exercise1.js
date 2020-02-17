5e0d84c76dea629eaa35d4e2c6b2bea041bb830e
I3enito <bensiegenthaler@gmail.com>
2020-02-17 14:34:49 +0100

    add color fill to rectangle

--- sw01/webgl_start/Exercise1.js
+++ sw01/webgl_start/Exercise1.js
@@ -12,72 +12,76 @@ var gl;
 
 // we keep all local parameters for the program in a single object with the name ctx (for context)
 var ctx = {
-  shaderProgram: -1,
-  vertexPositionId: -1,
+    shaderProgram: -1,
+    vertexPositionId: -1,
+    uColorId: -1,
 };
 
-var rectangleObject = { buffer: -1 };
+var rectangleObject = {buffer: -1};
 
 /**
  * Startup function to be called when the body is loaded
  */
 function startup() {
-  "use strict";
-  var canvas = document.getElementById("myCanvas");
-  gl = createGLContext(canvas);
-  initGL();
-  draw();
+    "use strict";
+    var canvas = document.getElementById("myCanvas");
+    gl = createGLContext(canvas);
+    initGL();
+    draw();
 }
 
 /**
  * InitGL should contain the functionality that needs to be executed only once
  */
 function initGL() {
-  "use strict";
-  ctx.shaderProgram = loadAndCompileShaders(
-    gl,
-    "VertexShader.glsl",
-    "FragmentShader.glsl"
-  );
-  setupAttributes();
-  setupBuffers();
+    "use strict";
+    ctx.shaderProgram = loadAndCompileShaders(
+        gl,
+        "VertexShader.glsl",
+        "FragmentShader.glsl"
+    );
+    setupAttributes();
+    setupBuffers();
 
-  // set the clear color here
-  gl.clearColor(0, 0, 1, 1);
+    // set the clear color here
+    gl.clearColor(0, 0, 1, 1);
 
-  // add more necessary commands here
+    // add more necessary commands here
 }
 
 /**
  * Setup the buffers to use. If more objects are needed this should be split into one file per object.
  */
 function setupBuffers() {
-  "use strict";
-  rectangleObject.buffer = gl.createBuffer();
-  var vertices = [0, 0, 0.5, 0, 0.5, 0.5, 0, 0.5];
-  gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    "use strict";
+    rectangleObject.buffer = gl.createBuffer();
+    var vertices = [0, 0, 0.5, 0, 0.5, 0.5, 0, 0.5];
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
 
 function setupAttributes() {
-  // finds the index of the variable in the program
-  ctx.vertexPositionId = gl.getAttribLocation(
-    ctx.shaderProgram,
-    "vertexPosition"
-  );
+    // finds the index of the variable in the program
+    ctx.vertexPositionId = gl.getAttribLocation(
+        ctx.shaderProgram,
+        "vertexPosition"
+    );
+    ctx.uColorId = gl.getUniformLocation(ctx.shaderProgram, "uColor");
 }
 
 /**
  * Draw the scene.
  */
 function draw() {
-  "use strict";
-  console.log("Drawing");
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  // add drawing routines here
-  gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
-  gl.vertexAttribPointer(ctx.vertexPositionId, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(ctx.vertexPositionId);
-  gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    "use strict";
+    console.log("Drawing");
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // add drawing routines here
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
+    gl.vertexAttribPointer(ctx.vertexPositionId, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(ctx.vertexPositionId);
+    gl.enableVertexAttribArray(ctx.uColorId);
+    gl.uniform4f(ctx.uColorId, 1.0, 1.0, 0.0, 1.0);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 }
